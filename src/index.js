3c0ee6c3e22d49e8801f0317c5bc170ae8b01538
Ali01 <alirahanet@gmail.com>
2023-03-09 17:06:50 +0430

    linter error solved

--- src/index.js
+++ src/index.js
@@ -3,35 +3,34 @@ import './style.css';
 const items = document.querySelector('.items');
 
 const data = [
-    {
+  {
 
-        description: "Reading Book",
-        completed: false,
-        index: 1,
-    },
-    {   description: "Buying Book",
-        completed: false,
-        index: 2,
-    },
-    {
-    description: "Coding with Partner",
+    description: 'Reading Book',
+    completed: false,
+    index: 1,
+  },
+  {
+    description: 'Buying Book',
+    completed: false,
+    index: 2,
+  },
+  {
+    description: 'Coding with Partner',
     completed: false,
     index: 3,
-    }
-  ];    
+  },
+];
 
-  function show() {
-    items.innerHTML = data.map((element) => (`
+function show() {
+  items.innerHTML = data.map((element) => (`
       <li class = "todo">
         <input type="checkbox" class="check"></input>
         <p class="item-text">${element.description}</p>
         <div class="moreBtn"></div>
       </li>`
-    )).join('');
-  }
-
+  )).join('');
+}
 
 window.onload = () => {
   show();
 };
-
