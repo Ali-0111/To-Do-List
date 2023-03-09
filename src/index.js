0fdaed9ff7407a54421193aceb254e1480cf9ba7
Ali01 <alirahanet@gmail.com>
2023-03-09 18:05:03 +0430

    sort function added

--- src/index.js
+++ src/index.js
@@ -4,25 +4,32 @@ const items = document.querySelector('.items');
 
 const data = [
   {
-
-    description: 'Reading Book',
-    completed: false,
-    index: 1,
-  },
-  {
     description: 'Buying Book',
     completed: false,
     index: 2,
   },
+
   {
     description: 'Coding with Partner',
     completed: false,
     index: 3,
   },
+
+  {
+
+    description: 'Reading Book',
+    completed: false,
+    index: 1,
+  },
+
 ];
 
 function show() {
-  items.innerHTML = data.map((element) => (`
+  // sort the array of objects by index
+
+  const sortedlist = data.sort((a, b) => a.index - b.index);
+
+  items.innerHTML = sortedlist.map((element) => (`
       <li class = "todo">
         <input type="checkbox" class="check"></input>
         <p class="item-text">${element.description}</p>
