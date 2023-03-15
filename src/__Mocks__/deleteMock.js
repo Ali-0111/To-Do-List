afffa4be989845009f4d00317f7a9f37816a6450
Ali01 <73581663+Ali-0111@users.noreply.github.com>
2023-03-16 01:48:04 +0430

    linters checked

--- src/__Mocks__/deleteMock.js
+++ src/__Mocks__/deleteMock.js
@@ -3,10 +3,10 @@ import localStorageMock from './localStorageMock.js';
 const deleteOnlyOne = (tasks, index) => {
   tasks.splice(index, 1);
   tasks.forEach((task, i) => {
-    task.index = i+1;
+    task.index = i + 1;
   });
   localStorageMock.saveToLocal('data', tasks);
   return tasks;
 };
 
-export {deleteOnlyOne};
+export default deleteOnlyOne;
